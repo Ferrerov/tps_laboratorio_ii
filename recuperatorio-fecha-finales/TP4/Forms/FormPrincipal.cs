05dbd7420b1c36063e55d54d27b71d1513f9aa97
Ferrerov <valenferrero01@gmail.com>
2022-07-18 08:19:21 -0700

    progreso

---
+++ recuperatorio-fecha-finales/TP4/Forms/FormPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Biblioteca;
+
+namespace Forms
+{
+    public partial class FormPrincipal : Form
+    {
+        List<Cliente> listaDeClientes;
+        public FormPrincipal()
+        {
+            InitializeComponent();
+            this.listaDeClientes = new List<Cliente>();
+        }
+
+        private void btnGestionarClientes_Click(object sender, EventArgs e)
+        {
+            FormGestionarClientes formGestionarClientes = new FormGestionarClientes(this.listaDeClientes);
+            formGestionarClientes.ShowDialog();
+            this.listaDeClientes = formGestionarClientes.listaDeClientes;
+            formGestionarClientes.Dispose();      
+        }
+    }
+}
