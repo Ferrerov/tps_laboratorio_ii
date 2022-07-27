a1fa90f38fc3d598a7b92b7a5a633c4d863e2338
Ferrerov <valenferrero01@gmail.com>
2022-07-27 10:10:54 -0300

    Entrega TP3

--- recuperatorio-fecha-finales/TP3/Archivos/IArchivos.cs
+++ recuperatorio-fecha-finales/TP3/Archivos/IArchivos.cs
@@ -4,8 +4,8 @@ namespace Archivos
 {
     public interface IArchivos<T>
     {
-        bool Guardar(string path, string nombre, T obj);
-
+        bool Guardar(string path, string nombre, T obj); 
+         
         bool Leer(string path, string nombre, out T obj);
     }
 }
